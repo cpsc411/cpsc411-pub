73e0f6f6d9922b28f924e4906f3930c15bb25c78
William J. Bowman <wjb@williamjbowman.com>
2021-02-05 18:14:03 -0800

    Add more undead-analysis tests for a3

--- cpsc411-lib/cpsc411/test-suite/public/a3.rkt
+++ cpsc411-lib/cpsc411/test-suite/public/a3.rkt
@@ -50,32 +50,129 @@
         [s2 expecteds]
         [i (in-naturals)])
     (unless (set=? s1 s2)
-      (fail-check (format "Expected undead set ~a for instruction ~a, but got ~a" i s2 s1)))))
+      (fail-check (format "Expected undead set ~a for instruction number ~a, but got ~a" s2 i s1)))))
 
 (define (a3-public-test-undead-sets undead-analysis)
-  (check-a3-undead-sets-equal?
-   (undead-analysis
-    '(module
-       ((locals (v.1 w.2 x.3 y.4 z.5 t.6 t.7 t.8 t.9)))
-       (begin
-         (set! v.1 1)
-         (set! w.2 46)
-         (set! x.3 v.1)
-         (set! t.7 7)
-         (set! x.3 (+ x.3 t.7))
-         (set! y.4 x.3)
-         (set! t.8 4)
-         (set! y.4 (+ y.4 t.8))
-         (set! z.5 x.3)
-         (set! z.5 (+ z.5 w.2))
-         (set! t.6 y.4)
-         (set! t.9 -1)
-         (set! t.6 (* t.6 t.9))
-         (set! z.5 (+ z.5 t.6))
-         (halt z.5))))
-   '((v.1) (v.1 w.2) (w.2 x.3) (t.7 w.2 x.3) (w.2 x.3) (y.4 w.2 x.3)
-           (t.8 y.4 w.2 x.3) (y.4 w.2 x.3) (z.5 y.4 w.2) (z.5 y.4) (t.6 z.5)
-           (t.6 z.5 t.9) (t.6 z.5) (z.5) ())))
+  (test-suite
+   "a3 undead-analysis test suite"
+
+   (test-case "Simple case with one variable"
+     (with-check-info (['undead-out (string-info "Undead of the form: (undead-out ((x.1) ()))")])
+       (check-match
+        (undead-analysis
+         '(module
+            ((locals (x.1)))
+            (begin
+              (set! x.1 5)
+              (halt x.1))))
+        `(module
+           ((locals (x.1)) (undead-out ,u))
+           (begin
+             (set! x.1 5)
+             (halt x.1)))
+        (validate-undead u '((x.1) ())))))
+
+   (test-case "Simple case with two variables"
+     (let ([e '((x.1) (x.1) ())])
+       (with-check-info (['undead-out (string-info (format "Undead of the form: (undead-out ~a)" e))])
+         (check-a3-undead-sets-equal?
+          (undead-analysis
+           '(module
+              ((locals (x.1 x.2)))
+              (begin
+                (set! x.1 5)
+                (set! x.2 10)
+                (halt x.1))))
+          e))))
+
+   (test-case "Simple case with two variables"
+     (let ([e '((x.1) (x.1 x.2) (x.2) ())])
+       (with-check-info (['undead-out (string-info (format "Undead of the form: (undead-out ~a)" e))])
+         (check-a3-undead-sets-equal?
+          (undead-analysis
+           '(module
+              ((locals (x.1 x.2)))
+              (begin
+                (set! x.1 5)
+                (set! x.2 10)
+                (set! x.1 (+ x.1 x.2))
+                (halt x.2))))
+          e))))
+
+   (test-case "Simple case with three variables"
+     (let ([e '((x.1) (x.1 x.2) (x.1 x.2 x.3) (x.2) ())])
+       (with-check-info (['undead-out (string-info (format "Undead of the form: (undead-out ~a)" e))])
+         (check-a3-undead-sets-equal?
+          (undead-analysis
+           '(module
+              ((locals (x.1 x.2 x.3)))
+              (begin
+                (set! x.1 5)
+                (set! x.2 10)
+                (set! x.3 x.2)
+                (set! x.3 (* x.3 x.1))
+                (halt x.2))))
+          e))))
+
+   (test-case "Complex case with three variables"
+     (let ([e '((x.1) (x.1 x.3) (x.3 x.1) (x.1) ())])
+       (with-check-info (['undead-out (string-info (format "Undead of the form: (undead-out ~a)" e))])
+         (check-a3-undead-sets-equal?
+          (undead-analysis
+           '(module
+              ((locals (x.1 x.2 x.3)))
+              (begin
+                (set! x.1 5)
+                (set! x.3 15)
+                (set! x.2 x.1)
+                (set! x.1 (* x.1 x.3))
+                (halt x.1))))
+          e))))
+
+   (test-case "Complex case with three variables"
+     (let ([e '((x.1) (x.1 x.2) (x.2) (x.3 x.2) (x.3 x.1) () (x.3) ())])
+       (with-check-info (['undead-out (string-info (format "Undead of the form: (undead-out ~a)" e))])
+         (check-a3-undead-sets-equal?
+          (undead-analysis
+           '(module
+              ((locals (x.1 x.2 x.3)))
+              (begin
+                (set! x.1 5)
+                (set! x.2 10)
+                (set! x.1 (+ x.1 x.1))
+                (set! x.3 15)
+                (set! x.1 x.2)
+                (set! x.1 (* x.1 x.3))
+                (set! x.3 5)
+                (halt x.3))))
+          e))))
+
+   (test-case "Big Program"
+     (let ([e '((v.1) (v.1 w.2) (w.2 x.3) (t.7 w.2 x.3) (w.2 x.3) (y.4 w.2 x.3)
+                (t.8 y.4 w.2 x.3) (y.4 w.2 x.3) (z.5 y.4 w.2) (z.5 y.4) (t.6 z.5)
+                (t.6 z.5 t.9) (t.6 z.5) (z.5) ())])
+       (with-check-info (['undead-out (string-info (format "Undead of the form: (undead-out ~a)" e))])
+         (check-a3-undead-sets-equal?
+          (undead-analysis
+           '(module
+              ((locals (v.1 w.2 x.3 y.4 z.5 t.6 t.7 t.8 t.9)))
+              (begin
+                (set! v.1 1)
+                (set! w.2 46)
+                (set! x.3 v.1)
+                (set! t.7 7)
+                (set! x.3 (+ x.3 t.7))
+                (set! y.4 x.3)
+                (set! t.8 4)
+                (set! y.4 (+ y.4 t.8))
+                (set! z.5 x.3)
+                (set! z.5 (+ z.5 w.2))
+                (set! t.6 y.4)
+                (set! t.9 -1)
+                (set! t.6 (* t.6 t.9))
+                (set! z.5 (+ z.5 t.6))
+                (halt z.5))))
+          e))))))
 
 (define (a3-assign-registers-stress-tests assign-registers)
   (define test-locals1 '(x.1 x.2 x.3))
