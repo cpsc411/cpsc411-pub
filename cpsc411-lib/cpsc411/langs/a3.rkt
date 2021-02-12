593b5bfaa42d7050968bcdccef80e42204ebe9e2
William J. Bowman <wjb@williamjbowman.com>
2021-02-12 12:51:54 -0800

    Rexport all a2 interpreters used in a3

--- cpsc411-lib/cpsc411/langs/a3.rkt
+++ cpsc411-lib/cpsc411/langs/a3.rkt
@@ -1,8 +1,10 @@
 #lang racket/base
 
-(provide (all-defined-out))
-
 (require "a2.rkt")
+
+(provide
+ (all-from-out "a2.rkt")
+ (all-defined-out))
 
 (define interp-asm-lang-v2/undead interp-asm-lang-v2)
 (define interp-asm-lang-v2/conflicts interp-asm-lang-v2)
