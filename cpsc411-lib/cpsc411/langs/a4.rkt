59390fb50e9ab6a55de72674a625aaf06b3c0344
William J. Bowman <wjb@williamjbowman.com>
2021-02-13 01:09:41 -0800

    Add the hard interpreters
    Added the a4 interpreters for the low-level languages with labels.
    Probably would have been easier to write these, but then I'd give away the game.

--- cpsc411-lib/cpsc411/langs/a4.rkt
+++ cpsc411-lib/cpsc411/langs/a4.rkt
@@ -53,4 +53,81 @@
 
 (define interp-block-asm-lang-v4 interp-block-pred-lang-v4)
 
-(define interp-para-asm-lang-v4 interp-block-asm-lang-v4)
+(module label-langs racket/base
+  (require
+   (submod ".." block-langs)
+   (for-syntax
+    racket/base
+    syntax/parse)
+   (only-in racket/base [module+ r:module+] [define r:define] [begin r:begin]))
+  (provide
+   (all-from-out (submod ".." block-langs))
+   (all-defined-out))
+
+  (r:define
+    flags
+    (make-hash
+     (list
+      (cons != #f)
+      (cons = #f)
+      (cons < #f)
+      (cons <= #f)
+      (cons > #f)
+      (cons >= #f))))
+
+  (define-syntax-rule (compare v1 v2)
+    (for-each (lambda (cmp)
+                (hash-set! flags cmp (cmp v1 v2)))
+              (list != = < <= > >=)))
+
+  (define-syntax-rule (jump-if flag d)
+    (r:begin
+     (when (hash-ref flags flag)
+       (d))))
+
+  (begin-for-syntax
+    (define (labelify-begin defs ss)
+      (syntax-parse ss
+        #:datum-literals (with-label)
+        [((with-label l s) ss ...)
+         (let-values ([(defs e) (labelify-begin defs #`(s ss ...))])
+           (values
+            (cons #`(r:define (l) #,e) defs)
+            #`(r:begin (l))))]
+        [(s ss ...)
+         (if (null? (attribute ss))
+             (values
+              defs
+              #`(r:begin s))
+             (let-values ([(defs e) (labelify-begin defs (attribute ss))])
+               (values
+                defs
+                #`(r:begin s #,e))))])))
+
+  (define-syntax (begin stx)
+    (let-values ([(defs e) (labelify-begin '() (cdr (syntax->list stx)))])
+      (datum->syntax
+       stx
+       (syntax->datum
+        #`(r:begin
+           (let/ec done
+             (r:begin
+              #,@defs
+              (r:define (halt v)
+               (set! rax v)
+               (done))
+              #,e))
+           rax)))))
+
+  (module+ interp
+    (provide interp-para-asm-lang-v4)
+    (define-namespace-anchor a)
+    (r:define interp-para-asm-lang-v4
+      (let ([ns (namespace-anchor->namespace a)])
+        (lambda (x)
+          (eval x ns))))))
+
+(require (submod 'label-langs interp))
+(provide interp-para-asm-lang-v4)
+
+(define interp-paren-x64-fvars-v4 interp-para-asm-lang-v4)
