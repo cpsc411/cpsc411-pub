fd70e302eef499261582c8ed50788879ff452e58
William J. Bowman <wjb@williamjbowman.com>
2021-02-12 18:54:40 -0800

    Add expose-basic-blocks tests to a4 test suite

--- cpsc411-lib/cpsc411/test-suite/public/a4.rkt
+++ cpsc411-lib/cpsc411/test-suite/public/a4.rkt
@@ -5,11 +5,65 @@
  rackunit
  cpsc411/test-suite/utils
  cpsc411/compiler-lib
+ "../../langs/a4.rkt"
  "a2.rkt"
  "a3.rkt")
 
 (provide (all-defined-out))
 
+
+(define (a4-expose-basic-blocks-test-suite passes expose-basic-blocks)
+  (define (check-ebb-correct actual source)
+    (check-correct interp-nested-asm-lang-v4 interp-block-asm-lang-v4 source actual))
+
+  (test-suite
+   "a4 expose-basic-blocks tests"
+
+   (let ([x `(module (begin (halt 5)))])
+     (fragile-test-case
+      (check-match (expose-basic-blocks x)
+                   `(module (define ,L.main.1 (begin (halt 5))))))
+
+     (check-ebb-correct (expose-basic-blocks x) x)
+
+     (check-from expose-basic-blocks passes x 5))
+
+   (let ([x `(module (begin (begin (halt 5))))])
+     (fragile-test-case
+      (check-match (expose-basic-blocks x)
+                   `(module (define ,L.main.1 (begin (halt 5))))))
+
+     (check-from expose-basic-blocks passes x 5))
+
+   (let ([x `(module
+               (begin (begin (set! fv0 1)
+                             (set! fv1 2))
+                      (set! fv0 (+ fv0 fv1))
+                      (halt fv0)))])
+     (check-match (expose-basic-blocks x)
+                  `(module
+                     (define ,L.main.1
+                       (begin
+                         (set! fv0 1)
+                         (set! fv1 2)
+                         (set! fv0 (+ fv0 fv1))
+                         (halt fv0)))))
+     (check-from expose-basic-blocks passes x 3))
+
+   (let ([x `(module
+               (begin (begin (set! fv0 1)
+                             (set! fv1 2))
+                      (begin (begin (set! fv0 (+ fv0 fv1))))
+                      (halt fv0)))])
+     (check-match (expose-basic-blocks x)
+                  `(module
+                     (define ,L.main.1
+                       (begin
+                         (set! fv0 1)
+                         (set! fv1 2)
+                         (set! fv0 (+ fv0 fv1))
+                         (halt fv0)))))
+     (check-from expose-basic-blocks passes x 3))))
 
 (define (a4-public-test-suite
          passes
@@ -62,10 +116,10 @@
 
     (a2-implement-fvars-test-suite passes implement-fvars)
     (a2-patch-instructions-test-suite passes patch-instructions)
-    ;(a2-flatten-begins-test-suite passes flatten-begins)
+    #;(a2-flatten-begins-test-suite passes flatten-program)
     (a2-select-instructions-test-suite passes select-instructions)
-    ;(a2-assign-homes-test-suite passes assign-homes-opt)
-    ;(a2-assign-homes-test-suite passes assign-homes)
+    #;(a2-assign-homes-test-suite passes assign-homes-opt)
+    #;(a2-assign-homes-test-suite passes assign-homes)
     (a2-canonicalize-bind-test-suite passes canonicalize-bind)
     (a2-sequentialize-let-test-suite passes sequentialize-let)
     (a2-uniquify-test-suite passes uniquify)
@@ -74,4 +128,10 @@
 
     (a3-public-test-undead-sets undead-analysis)
     (a3-conflict-analysis-tests conflict-analysis)
-    (a3-assign-registers-stress-tests assign-registers))))
+    (a3-assign-registers-stress-tests assign-registers))
+
+   (a4-expose-basic-blocks-test-suite passes expose-basic-blocks)))
+
+;; Local Variables:
+;; eval: (put 'module 'racket-indent-function 0)
+;; End:
