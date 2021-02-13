9ba50ec8ae9e8ed591c03474aee024c99527a031
William J. Bowman <wjb@williamjbowman.com>
2021-02-13 01:15:25 -0800

    Add a bunch of low-level tests for a4

--- cpsc411-lib/cpsc411/test-suite/public/a4.rkt
+++ cpsc411-lib/cpsc411/test-suite/public/a4.rkt
@@ -11,6 +11,424 @@
 
 (provide (all-defined-out))
 
+(define (test-eq-normal m f e o)
+  (let ([s string-normalize-spaces])
+    (test-case m (check-equal? (s (f e)) (s o)))))
+
+(define (a4-link-paren-x64-test-suite link-paren-x64)
+  (test-suite
+   "a4 link-paren-x64 test suite"
+
+   (test-case "Single label case"
+     (check-match
+      (link-paren-x64
+       '(begin
+          (with-label L.test.1 (set! (rbp - 0) 8))
+          (set! (rbp - 8) 0)
+          (set! rax (rbp - 0))
+          (set! rax (* rax (rbp - 0)))))
+      '(begin
+         (set! (rbp - 0) 8)
+         (set! (rbp - 8) 0)
+         (set! rax (rbp - 0))
+         (set! rax (* rax (rbp - 0))))))
+
+   (test-case "Single label case with jump"
+     (check-match
+      (link-paren-x64
+       '(begin
+          (with-label L.test.1 (set! r9 7))
+          (jump L.test.1)))
+      '(begin
+         (set! r9 7)
+         (jump 0))))
+
+   (test-case "Multiple label case with jumps"
+     (check-match
+      (link-paren-x64
+       '(begin
+          (set! rsp 1)
+          (with-label L.test.1 (set! rax 1))
+          (set! rbx 2)
+          (jump L.test.2)
+          (set! rdi 2)
+          (with-label L.test.2 (set! rsi 2))
+          (jump L.test.1)))
+      '(begin
+         (set! rsp 1)
+         (set! rax 1)
+         (set! rbx 2)
+         (jump 5)
+         (set! rdi 2)
+         (set! rsi 2)
+         (jump 1))))
+
+   (test-case "Complex nested label case"
+     (check-match
+      (link-paren-x64
+       '(begin
+          (set! rax L.link.1)
+          (with-label L.link.1
+            (jump L.link.2))
+          (with-label L.link.2
+            (compare rbx 3))
+          (with-label L.link.3
+            (jump-if = L.link.4))
+          (with-label L.link.4
+            (set! rax L.link.2))
+          (jump L.link.1)))
+      '(begin
+         (set! rax 1)
+         (jump 2)
+         (compare rbx 3)
+         (jump-if = 4)
+         (set! rax 2)
+         (jump 1))))))
+
+(define (a4-interp-paren-x64-test-suite interp-paren-x64)
+  (test-suite
+   "a4 interp-paren-x64 test suite"
+
+   (let ([e `(begin
+               (set! rdi L.label.1)
+               (set! rax 1)
+               (jump rdi)
+               (set! rax 10)
+               (with-label L.label.1
+                 (set! rax (+ rax 100))))])
+     (test-case "Simple single label case with jump"
+       (check-equal? (interp-paren-x64 e) 101)))
+
+   (let ([e `(begin
+               (set! rbx 4)
+               (compare rbx 4)
+               (jump-if < L.testing.1)
+               (set! rbx -66)
+               (jump L.end.1)
+               (with-label L.testing.1 (set! rbx 11))
+               (with-label L.end.1 (set! rax rbx))
+               (set! rax rax))])
+     (test-case "Moderate single label case with jump"
+       (check-equal? (interp-paren-x64 e) -66)))
+
+   (test-case "Moderate single label case with jump"
+     (check-equal?
+      (interp-paren-x64
+       `(begin
+          (set! (rbp - 16) -1)
+          (set! r10 0)
+          (set! rax 10)
+          (with-label L.x.1 (set! rax (+ rax (rbp - 16))))
+          (compare rax r10)
+          (jump-if > L.x.1)))
+      0))
+
+   (let ([e '(begin
+               (with-label L.cs411main.1
+                 (set! r11 7))
+               (with-label L.fib.1
+                 (set! r9 0))
+               (set! r10 1)
+               (set! r12 r11)
+               (with-label L.fib_iter.1
+                 (compare r12 0))
+               (jump-if = L.fib_done.1)
+               (with-label L.fib_not_done_yet.1
+                 (set! r13 r10))
+               (set! r10 (+ r10 r9))
+               (set! r9 r13)
+               (set! r12 (+ r12 -1))
+               (jump L.fib_iter.1)
+               (with-label L.fib_done.1
+                 (set! rdi r10))
+               (set! rax rdi))])
+     (test-case "Complex case"
+       (check-equal? (interp-paren-x64 e) 21)))))
+
+(define (a4-generate-x64-test-suite passes interp-paren-x64 generate-x64)
+  (test-suite
+   "a4 generate-x64 test suite"
+
+   (let ([x '(begin
+               (set! rdx 4)
+               (with-label L.x.1 (set! rdx (+ rdx -111)))
+               (set! rax rdx))])
+     (test-eq-normal
+      "Simple label case"
+      generate-x64
+      x
+      "mov rdx, 4
+ L.x.1:
+ add rdx, -111
+ mov rax, rdx")
+
+     (check-equal? (interp-paren-x64 x) -107)
+
+     (check-from generate-x64 passes x -107))
+
+   (let ([x '(begin
+               (set! (rbp - 0) 21)
+               (set! rax 0)
+               (set! rsp 1)
+               (with-label L.test.1 (set! (rbp - 8) 2))
+               (compare rax rsp)
+               (jump-if = L.test.1)
+               (set! rax (rbp - 0))
+               (set! rax (* rax (rbp - 8))))])
+
+     (test-eq-normal
+      "Moderate label case"
+      generate-x64
+      x
+      "mov QWORD [rbp - 0], 21
+       mov rax, 0
+       mov rsp, 1
+  L.test.1:
+  mov QWORD [rbp - 8], 2
+  cmp rax, rsp
+  je L.test.1
+  mov rax, QWORD [rbp - 0]
+  imul rax, QWORD [rbp - 8]")
+
+     (check-equal? (interp-paren-x64 x) 42)
+
+     (check-from generate-x64 passes x 42))
+
+   (let ([x '(begin
+               (set! rdx 4)
+               (set! rcx L.testing.4)
+               (with-label L.testing.1 (jump L.testing.3))
+               (with-label L.testing.2 (jump L.testing.1))
+               (with-label L.testing.3 (jump rcx))
+               (with-label L.testing.4 (set! rax rdx)))])
+     (test-eq-normal
+      "Complex case"
+      generate-x64
+      x
+      "mov rdx, 4
+       mov rcx, L.testing.4
+L.testing.1:
+jmp L.testing.3
+L.testing.2:
+jmp L.testing.1
+L.testing.3:
+jmp rcx
+L.testing.4:
+mov rax, rdx")
+
+     (check-equal? (interp-paren-x64 x) 4)
+
+     (check-from generate-x64 passes x 4))))
+
+(define (a4-patch-instructions-test-suite passes patch-instructions)
+  (define-check (check-patch-instructions x)
+    (check-correct interp-para-asm-lang-v4 interp-paren-x64-fvars-v4
+                   x
+                   (patch-instructions x)))
+  (test-suite
+   "a4 patch-instructions tests"
+
+   (test-case "Simple label case without jump"
+     (let ([x '(begin
+                 (set! rsi L.label.1)
+                 (with-label L.label.1
+                   (set! rbx 18))
+                 (halt rbx))])
+       (check-match
+        (patch-instructions x)
+        `(begin
+           (set! rsi ,l1)
+           (with-label ,l1
+             (set! rbx 18))
+           (set! rax rbx)
+           (jump done)))
+
+       (check-patch-instructions x)))
+
+
+   (test-case "Simple label case with jump"
+     (let ([x '(begin
+                 (set! rcx L.label.1)
+                 (jump rcx)
+                 (with-label L.label.1
+                   (set! rcx 42))
+                 (halt rcx))])
+       (check-match
+        (patch-instructions x)
+        `(begin
+           (set! rcx L.label.1)
+           (jump rcx)
+           (with-label L.label.1
+             (set! rcx 42))
+           (set! rax rcx)
+           (jump done)))
+
+       (check-patch-instructions x)))
+
+   (test-case "Moderate single label case"
+     (let ([x '(begin
+                 (set! rdx 9)
+                 (set! rcx 18)
+                 (set! rsi L.label.1)
+                 (compare rdx rcx)
+                 (jump-if < L.label.1)
+                 (with-label L.label.1
+                   (set! rdx 27))
+                 (halt rdx))])
+       (check-match
+        (patch-instructions x)
+        `(begin
+           (set! rdx 9)
+           (set! rcx 18)
+           (set! rsi L.label.1)
+           (compare rdx rcx)
+           (jump-if < L.label.1)
+           (with-label L.label.1
+             (set! rdx 27))
+           (set! rax rdx)
+           (jump done)))
+
+       (check-patch-instructions x)))
+
+   ; or checks could be made more precise
+   (test-case "Complex multiple label case"
+     (let ([x '(begin
+                 (with-label L.main.51 (set! r14 1))
+                 (set! r15 5)
+                 (with-label L.fact_loop.50 (compare r15 0))
+                 (jump-if = L.nested.54)
+                 (set! r13 r15)
+                 (set! r15 (+ r15 -1))
+                 (set! r14 (* r14 r13))
+                 (jump L.fact_loop.50)
+                 (with-label L.nested.54 (halt r14)))])
+       (check-match
+        (patch-instructions x)
+        `(begin
+           (with-label L.main.51 (set! r14 1))
+           (set! r15 5)
+           (with-label L.fact_loop.50 (compare r15 0))
+           (jump-if = L.nested.54)
+           (set! r13 r15)
+           (set! r15 (+ r15 -1))
+           (set! r14 (* r14 r13))
+           (jump L.fact_loop.50)
+           (with-label L.nested.54 (set! rax r14))
+           (jump done)))
+
+       (check-patch-instructions x)))))
+
+(define (a4-flatten-program-test-suite passes flatten-program)
+  (test-suite
+   "a4 flatten-program tests"
+
+   (test-case "Simple case"
+     (check-equal?
+      (flatten-program
+       '(module
+          (define L.x.1
+            (halt 123))))
+      '(begin
+         (with-label L.x.1
+           (halt 123)))))
+
+   (test-case "Simple case with jump"
+     (check-equal?
+      (flatten-program
+       '(module
+          (define L.x.0
+            (begin
+              (set! rsi 123)
+              (set! rsi (+ rsi 1))
+              (jump L.x.1)))
+          (define L.x.1
+            (halt rsi))))
+      '(begin
+         (with-label L.x.0
+           (set! rsi 123))
+         (set! rsi (+ rsi 1))
+         (jump L.x.1)
+         (with-label L.x.1
+           (halt rsi)))))
+
+   (test-case "Simple case with two blocks"
+     (check-equal?
+      (flatten-program
+       '(module
+          (define L.x.1
+            (begin
+              (set! rcx rcx)
+              (jump L.x.1)))
+          (define L.x.2
+            (begin
+              (set! rcx rcx)
+              (halt 0)))))
+      '(begin
+         (with-label L.x.1 (set! rcx rcx))
+         (jump L.x.1)
+         (with-label L.x.2 (set! rcx rcx))
+         (halt 0))))
+
+   (test-case "Complex case"
+     (check-equal?
+      (flatten-program
+       `(module
+          (define L.tmp.1
+            (begin
+              (set! rdx 42)
+              (begin
+                (set! rdx 20)
+                (begin
+                  (set! rdx (+ rdx rdx))
+                  (jump L.tmp.2)))))
+          (define L.tmp.2
+            (begin
+              (halt rdx)))))
+      `(begin
+         (with-label L.tmp.1
+           (set! rdx 42))
+         (set! rdx 20)
+         (set! rdx (+ rdx rdx))
+         (jump L.tmp.2)
+         (with-label L.tmp.2
+           (halt rdx)))))
+
+   (test-case "Complex case"
+     (check-equal?
+      (flatten-program
+       `(module
+          (define
+            L.tmp.1
+            (begin
+              (set! rdx 42)
+              (if (> rdx 43) (jump L.tmp.2) (jump L.tmp.3))))
+          (define
+            L.tmp.2
+            (begin
+              (set! rdx 30)
+              (jump L.tmp.4)))
+          (define
+            L.tmp.3
+            (begin
+              (set! rdx 0)
+              (jump L.tmp.4)))
+          (define
+            L.tmp.4
+            (halt rdx))))
+      `(begin
+         (with-label L.tmp.1
+           (set! rdx 42))
+         (compare rdx 43)
+         (jump-if > L.tmp.2)
+         (jump L.tmp.3)
+         (with-label L.tmp.2
+           (set! rdx 30))
+         (jump L.tmp.4)
+         (with-label L.tmp.3
+           (set! rdx 0))
+         (jump L.tmp.4)
+         (with-label L.tmp.4
+           (halt rdx)))))))
 
 (define (a4-expose-basic-blocks-test-suite passes expose-basic-blocks)
   (define (check-ebb-correct actual source)
@@ -137,6 +555,11 @@
     (a3-conflict-analysis-tests conflict-analysis)
     (a3-assign-registers-stress-tests assign-registers))
 
+   (a4-link-paren-x64-test-suite link-paren-x64)
+   (a4-interp-paren-x64-test-suite interp-paren-x64)
+   (a4-generate-x64-test-suite passes interp-paren-x64 generate-x64)
+   (a4-patch-instructions-test-suite passes patch-instructions)
+   (a4-flatten-program-test-suite passes flatten-program)
    (a4-expose-basic-blocks-test-suite passes expose-basic-blocks)))
 
 ;; Local Variables:
