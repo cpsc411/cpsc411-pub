09166b6b63e1ec5142a883df7f14c18006bb93ba
William J. Bowman <wjb@williamjbowman.com>
2021-02-12 18:54:24 -0800

    Add additiona a4 interpreters

--- cpsc411-lib/cpsc411/langs/a4.rkt
+++ cpsc411-lib/cpsc411/langs/a4.rkt
@@ -18,4 +18,39 @@
 (define interp-asm-pred-lang-v4/undead interp-asm-pred-lang-v4/locals)
 (define interp-asm-pred-lang-v4/conflicts interp-asm-pred-lang-v4/undead)
 (define interp-asm-pred-lang-v4/assignments interp-asm-pred-lang-v4/conflicts)
-(define interp-nested-asm-lang-v4 interp-asm-pred-lang-v4/assignments)
+(define interp-nested-asm-lang-v4 interp-values-lang-v3)
+
+(module block-langs racket/base
+  (require
+   "base.rkt"
+   (only-in racket/base [module+ r:module+] [define r:define]))
+  (provide
+   (all-from-out "base.rkt")
+   (all-defined-out))
+
+  (define-syntax-rule (define l tail)
+    (r:define (l) tail))
+
+  (define-syntax-rule (module (define l tail) b ...)
+    (begin
+      (define l tail)
+      b ...
+      (l)))
+
+  (define-syntax-rule (jump l)
+    (l))
+
+  (r:module+ interp
+    (provide interp-block-pred-lang-v4)
+    (define-namespace-anchor a)
+    (r:define interp-block-pred-lang-v4
+      (let ([ns (namespace-anchor->namespace a)])
+        (lambda (x)
+          (eval x ns))))))
+
+(require (submod 'block-langs interp))
+(provide interp-block-pred-lang-v4)
+
+(define interp-block-asm-lang-v4 interp-block-pred-lang-v4)
+
+(define interp-para-asm-lang-v4 interp-block-asm-lang-v4)
