5a56df1df6672cf291dfa09d0ee45317068ef647
William J. Bowman <wjb@williamjbowman.com>
2021-03-05 23:23:23 -0800

    Add emacs dir-locals file

---
+++ cpsc411-lib/cpsc411/test-suite/public/.dir-locals.el
@@ -0,0 +1,4 @@
+((emacs-lisp-mode . ((eval . (put 'for/foldr 'racket-indent-function 2))
+                     (eval . (put 'module 'racket-indent-function 0) )
+                     (eval . (put 'return-point 'racket-indent-function 1))))
+ (scribble-mode . ((eval . (put 'nd-match 'racket-indent-function 1)))))
