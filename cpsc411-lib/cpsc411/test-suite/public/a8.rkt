4ddcec2a5a402862780a9f79fe51a9ca34d7292a
William J. Bowman <wjb@williamjbowman.com>
2021-03-26 18:31:57 -0700

    Add initial a8 test suite

---
+++ cpsc411-lib/cpsc411/test-suite/public/a8.rkt
@@ -0,0 +1,120 @@
+#lang reader "../../test-lang/lang/reader.rkt"
+
+(require
+ racket/match
+ (for-syntax
+  racket/function
+  racket/syntax)
+ rackunit
+ cpsc411/test-suite/utils
+ cpsc411/compiler-lib
+ cpsc411/ptr-run-time
+ ;"../../langs/a8.rkt"
+ "a7.rkt"
+ )
+
+(provide (all-defined-out))
+
+(define (a8-public-test-suite
+         passes
+
+         check-exprs-lang
+         uniquify
+         implement-safe-primops
+         specify-representation
+         remove-complex-opera*
+         sequentialize-let
+         impose-calling-conventions
+         canonicalize-bind
+         select-instructions
+         expose-allocation-pointer
+         uncover-locals
+         undead-analysis
+         conflict-analysis
+         assign-call-undead-variables
+         allocate-frames
+         assign-registers
+         assign-frame-variables
+         replace-locations
+         optimize-predicates
+         implement-fvars
+         expose-basic-blocks
+         resolve-predicates
+         flatten-program
+         patch-instructions
+         implement-mops
+         generate-x64)
+
+  (define decoder (current-actual-decoder))
+  (define masker (current-expected-masker))
+  (define encoder (current-input-encoder))
+  (define run/read (current-run/read))
+
+  (test-suite
+   "a7 public test suite"
+
+   #:before
+   (thunk
+    (current-run/read nasm-run/read))
+
+   #:after
+   (thunk
+    (current-run/read run/read))
+
+   (a7-public-test-suite
+    (current-pass-list)
+
+    values
+
+    check-exprs-lang
+    uniquify
+    implement-safe-primops
+    specify-representation
+    remove-complex-opera*
+    sequentialize-let
+    impose-calling-conventions
+    canonicalize-bind
+    select-instructions
+    uncover-locals
+    undead-analysis
+    conflict-analysis
+    assign-call-undead-variables
+    allocate-frames
+    assign-registers
+    assign-frame-variables
+    replace-locations
+    optimize-predicates
+    implement-fvars
+    expose-basic-blocks
+    resolve-predicates
+    flatten-program
+    patch-instructions
+    generate-x64)
+
+   #;(make-tests
+    passes
+    generate-x64
+    patch-instructions
+    flatten-program
+    resolve-predicates
+    expose-basic-blocks
+    optimize-predicates
+    implement-fvars
+    assign-frame-variables
+    replace-locations
+    assign-registers
+    allocate-frames
+    assign-call-undead-variables
+    conflict-analysis
+    undead-analysis
+    uncover-locals
+    select-instructions
+    canonicalize-bind
+    impose-calling-conventions
+    sequentialize-let
+    remove-complex-opera*
+    specify-representation
+    implement-safe-primops)
+
+   #;(a8-end-to-end-test-suite passes)
+   #;(a8-void-test-suite passes)))
