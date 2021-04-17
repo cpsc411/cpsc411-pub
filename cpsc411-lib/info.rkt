54eb7db3481a0e4b255b3a4e0a7127fcd3d5abc0
William J. Bowman <wjb@williamjbowman.com>
2021-04-16 18:11:21 -0700

    Fix typo in dependency path

--- cpsc411-lib/info.rkt
+++ cpsc411-lib/info.rkt
@@ -2,7 +2,7 @@
 (define collection 'multi)
 (define deps
   '("rackunit-lib"
-    "https://github.com/wilbowma/memoize?path=memoize-lib"
+    "https://github.com/wilbowma/memoize.git?path=memoize-lib"
     ("base" #:version "7.3")))
 (define build-deps
   '("at-exp-lib"
