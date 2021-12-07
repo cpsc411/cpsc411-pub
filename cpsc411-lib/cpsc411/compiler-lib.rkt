0c694a3d87077b2717ff0f2830d0e6332e2208eb
William J. Bowman <wjb@williamjbowman.com>
2021-12-07 15:04:27 -0800

    Disable PIE on macOS so we can use mov instead of lea

--- cpsc411-lib/cpsc411/compiler-lib.rkt
+++ cpsc411-lib/cpsc411/compiler-lib.rkt
@@ -284,7 +284,8 @@
 ; A string representing additional ld flags for this operating system.
 (define (ld-flags [type (system-type)])
   (match type
-    ['macosx (list "-macosx_version_min" "10.6" "-e" "start")]
+    ;; -no_pie needed until we switch from mov to lea for static pointers
+    ['macosx (list "-no_pie" "-macosx_version_min" "10.6" "-e" "start")]
     [_ (list "-e" "start")]))
 
 ; String
