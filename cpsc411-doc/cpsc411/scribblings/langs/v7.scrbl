9090e1cdcf09418db9c5145d36d69f2277427451
William J. Bowman <wjb@williamjbowman.com>
2022-03-07 18:34:42 -0800

    Split algebraic expressions into v6.5

---
+++ cpsc411-doc/cpsc411/scribblings/langs/v7.scrbl
@@ -0,0 +1,37 @@
+#lang scribble/manual
+
+@(require
+"utils.rkt"
+cpsc411/langs/v7
+(for-label cpsc411/langs/v7)
+(for-label racket/contract))
+
+@title{v7 Languages}
+@declare-exporting[cpsc411/langs/v7]
+
+@deflangs[
+exprs-lang-v7
+exprs-unique-lang-v7
+exprs-unsafe-data-lang-v7
+exprs-bits-lang-v7
+exprs-bits-lang-v7/contexts
+values-bits-lang-v7
+proc-imp-mf-lang-v7
+imp-mf-lang-v7
+imp-cmf-lang-v7
+asm-pred-lang-v7
+asm-pred-lang-v7/locals
+asm-pred-lang-v7/undead
+asm-pred-lang-v7/conflicts
+asm-pred-lang-v7/pre-framed
+asm-pred-lang-v7/framed
+asm-pred-lang-v7/spilled
+asm-pred-lang-v7/assignments
+nested-asm-lang-fvars-v7
+nested-asm-lang-v7
+block-pred-lang-v7
+block-asm-lang-v7
+para-asm-lang-v7
+paren-x64-v7
+paren-x64-rt-v7
+]
---
+++ cpsc411-lib/cpsc411/langs/v6-5.rkt
@@ -0,0 +1,104 @@
+#lang at-exp racket/base
+
+(require
+ cpsc411/compiler-lib
+ cpsc411/info-lib
+ scribble/bettergrammar
+ racket/contract
+ (for-label cpsc411/info-lib)
+ (for-label racket/contract)
+ (for-label cpsc411/compiler-lib)
+ "redex-gen.rkt"
+ (submod "base.rkt" interp)
+ "v6.rkt")
+
+(provide (all-defined-out))
+
+@define-grammar/pred[exprs-lang-v6.5
+#:literals (name? int61? uint8? ascii-char-literal?)
+#:datum-literals (module lambda define let if void error * + - eq? < <= >
+                  >= fixnum? boolean? empty? void? ascii-char? error? not
+                  call empty)
+[p     (module (define x (lambda (x ...) value)) ... value)]
+[pred  (relop triv triv)
+       (true)
+       (false)
+       (not pred)
+       (let ([x value] ...) pred)
+       (if pred pred pred)]
+[value triv
+       (binop value value)
+       (let ([x value] ...) value)
+       (if pred value value)
+       (call x value ...)]
+[triv  int64 x]
+[x     name?]
+[binop * + -]
+[relop < <= = >= > !=]
+[int64 int64?]
+]
+
+(define (interp-exprs-lang-v6.5 x)
+  (interp-base x))
+
+@define-grammar/pred[exprs-unique-lang-v6.5
+#:literals (name? int61? uint8? ascii-char-literal?)
+#:datum-literals (module lambda define let if void error * + - eq? < <= >
+                         >= fixnum? boolean? empty? void? ascii-char? error? not
+                         call empty)
+[p     (module (define label (lambda (aloc ...) value)) ... value)]
+[pred  (relop opand opand)
+       (true)
+       (false)
+       (not pred)
+       (let ([aloc e] ...) pred)
+       (if pred pred pred)]
+[value triv
+       (binop value value)
+       (call triv value ...)
+       (let ([aloc value] ...) value)
+       (if pred value value)]
+[opand int64 aloc]
+[triv  opand label]
+[binop * + -]
+[relop < <= = >= > !=]
+[aloc  aloc?]
+[label label?]
+[int64 int64?]
+]
+
+(define (interp-exprs-unique-lang-v6.5 x)
+  (interp-base x))
+
+@define-grammar/pred[exprs-unique-lang-v6.5/context
+#:literals (aloc? label? int64?)
+#:datum-literals (module lambda define apply let if void error * + - = != < <= >
+                  >=  bitwise-and bitwise-ior bitwise-xor
+                  arithmetic-shift-right true false not call)
+[p     (module (define label (lambda (aloc ...) tail)) ... tail)]
+[pred  (relop opand opand)
+       (true)
+       (false)
+       (not pred)
+       (let ([aloc value] ...) pred)
+       (if pred pred pred)]
+[tail  value
+       (let ([aloc value] ...) tail)
+       (if pred tail tail)
+       (call triv value ...)]
+[value triv
+       (binop value value)
+       (let ([aloc value] ...) value)
+       (if pred value value)
+       (call triv value ...)]
+[opand int64 aloc]
+[triv opand label]
+[binop * + -]
+[relop < <= = >= > !=]
+[aloc  aloc?]
+[label label?]
+[int64 int64?]
+]
+
+(define (interp-exprs-unique-lang-v6.5/context x)
+  (interp-base x))
