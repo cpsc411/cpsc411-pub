1f3d5924a140d56e153d1f64ca8c85817deaf90f
William J. Bowman <wjb@williamjbowman.com>
2025-02-14 11:47:40 -0800

    Fix dynamic test generation in testomatic framework

--- cpsc411-lib/cpsc411/test-suite/utils.rkt
+++ cpsc411-lib/cpsc411/test-suite/utils.rkt
@@ -398,46 +398,47 @@
              #:when (eq? interp interp-src))
     pass))
 
+; NOTE: This function modifies the test suite dynamically.
+; It must be imperative, and executed during the dynamic extent of a test suite, since it is mutating the registered tests during test execution.
+; It MUST NOT return a test suite.
 (define (test-compiler-pass pass src-interp trg-interp trg-validator
                             [src-equiv equal?]
                             #:grading [grading? #f])
   (when (eq? trg-interp interp-base)
     (error "interp-base used as target interpreter"))
   (define target-interp-progs (hash-ref! test-prog-dict trg-interp (mutable-set)))
-  (make-test-suite (format "~a suite" (or (object-name pass) 'anonymous))
-    (for/list ([test-prog-entry (hash-ref! test-prog-dict src-interp (mutable-set))]
-               [i (in-naturals)])
-      (define name (first test-prog-entry))
-      (define test-prog (second test-prog-entry))
-      (define expected (third test-prog-entry))
-      (test-suite (format "Suite for entry ~a" i)
-        (with-check-info (['test-program test-prog]
-                          ['expected expected]
-                          ['src-interp (object-name src-interp)]
-                          ['trg-interp (object-name trg-interp)])
-          (test-begin
-            (define _output (box (void)))
-            (with-check-info (['test-type "Checking test-program compiles without error"])
-              (check-not-exn
-               (thunk
-                (with-timeout
-                  (set-box! _output (pass test-prog))))))
-            (define output (unbox _output))
-            (with-check-info (['output-program output])
-              (with-check-info (['test-type "Checking that output is syntactically correct"])
-                (when trg-validator
-                  (check-true (trg-validator output))))
-              (define actual (box (void)))
-              (with-check-info (['test-type "Checking output is interpretable"])
-                (check-not-exn
-                 (thunk (with-timeout
-                          (set-box! actual (trg-interp output))))))
-              (with-check-info (['test-type "Checking that output produces correct value"])
-                (check src-equiv expected (unbox actual)))
-              ; if grading, disable dynamic generation.
-              ; these are useful for debugging, since they isolate bugs, but screw up test count for assessment.
-              (unless grading?
-                (set-add! target-interp-progs `(,name ,output ,(unbox actual)))))))))))
+  (for ([test-prog-entry (hash-ref! test-prog-dict src-interp (mutable-set))]
+        [i (in-naturals)])
+    (define name (first test-prog-entry))
+    (define test-prog (second test-prog-entry))
+    (define expected (third test-prog-entry))
+    (test-case (format "Suite for entry ~a" i)
+      (with-check-info (['test-program test-prog]
+                        ['expected expected]
+                        ['src-interp (object-name src-interp)]
+                        ['trg-interp (object-name trg-interp)])
+         (define _output (box (void)))
+         (with-check-info (['test-type "Checking test-program compiles without error"])
+           (check-not-exn
+            (thunk
+             (with-timeout
+               (set-box! _output (pass test-prog))))))
+         (define output (unbox _output))
+         (with-check-info (['output-program output])
+           (with-check-info (['test-type "Checking that output is syntactically correct"])
+             (when trg-validator
+               (check-true (trg-validator output))))
+           (define actual (box (void)))
+           (with-check-info (['test-type "Checking output is interpretable"])
+             (check-not-exn
+              (thunk (with-timeout
+                       (set-box! actual (trg-interp output))))))
+           (with-check-info (['test-type "Checking that output produces correct value"])
+             (check src-equiv expected (unbox actual)))
+           ; if grading, disable dynamic generation.
+           ; these are useful for debugging, since they isolate bugs, but screw up test count for assessment.
+           (unless grading?
+             (set-add! target-interp-progs `(,name ,output ,(unbox actual)))))))))
 
 ; Disables fragile/feedback-only tests, typically for grading but also if you
 ; just want to.
@@ -515,7 +516,8 @@
                       expected
                       (parameterize ([current-pass-list pass-ls])
                         (execute test-prog run/read)))))))
-            (test-compiler-pass pass src-interp trg-interp trg-validator src-equiv))
+            (test-suite ""
+              (test-compiler-pass pass src-interp trg-interp trg-validator src-equiv)))
           (loop (rest pass-ls) (rest interp-ls)))]))))
 
 
